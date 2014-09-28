6a1ae878b21bf5221efd4f394f520c4ced3bca95
Steven Yencho <onethathasnolife@gmail.com>
2014-09-28 16:20:41 -0400

    Move File Method

--- src/Compiler.java
+++ src/Compiler.java
@@ -110,4 +110,40 @@ public class Compiler
 	    }
 	    return success;
 	  }
+	  
+	  public void moveFile(String filePath, String toFilePath){
+		  	InputStream inStream = null;
+			OutputStream outStream = null;
+		 
+		    	try{
+		 
+		    	    File afile =new File(filePath);
+		    	    File bfile =new File(toFilePath);
+		 
+		    	    inStream = new FileInputStream(afile);
+		    	    outStream = new FileOutputStream(bfile);
+		 
+		    	    byte[] buffer = new byte[1024];
+		 
+		    	    int length;
+		    	    //copy the file content in bytes 
+		    	    while ((length = inStream.read(buffer)) > 0){
+		 
+		    	    	outStream.write(buffer, 0, length);
+		 
+		    	    }
+		 
+		    	    inStream.close();
+		    	    outStream.close();
+		 
+		    	    //delete the original file
+		    	    afile.delete();
+		 
+		    	    System.out.println("File is copied successful!");
+		 
+		    	}catch(IOException e){
+		    	    e.printStackTrace();
+		    	}
+		  
+	  }
 	}
